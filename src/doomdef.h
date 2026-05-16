5bb0a1b80cb9597fae2575bfebb2c45e6c0b3954
supercoolsonic <joeyj95@hotmail.com>
2026-05-15 21:16:36 -0400

    16 player cap branch test
    Temporary 16 player cap so that online play is stable for those that want to play online.

--- src/doomdef.h
+++ src/doomdef.h
@@ -172,8 +172,8 @@ extern char logfilename[1024];
 // The maximum number of players, multiplayer/networking.
 // NOTE: it needs more than this to increase the number of players...
 
-//#define MAXPLAYERS 16
-#define MAXPLAYERS 32					//SCS EDIT - Let's try 32 players?
+#define MAXPLAYERS 16
+//#define MAXPLAYERS 32					//SCS EDIT - Let's try 32 players?
 #define PLAYERSMASK (MAXPLAYERS-1)
 #define MAXPLAYERNAME 21
 #define MAXSPLITSCREENPLAYERS 4 // Max number of players on a single computer
